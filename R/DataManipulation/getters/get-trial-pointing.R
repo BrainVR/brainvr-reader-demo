f179906db4225cdd33e4f0b3d77db5190d3142c4
Lukáš Hejtmánek <hejtmy@gmail.com>
2017-10-30 10:51:27 -0700

    Adds namespaced trial pointing

--- R/DataManipulation/getters/get-trial-pointing.R
+++ R/DataManipulation/getters/get-trial-pointing.R
@@ -1,21 +1,23 @@
-get_trial_pointing <- function(dt_player, test, trialID){
+get_trial_pointing <- function(dt_player, test, trialID, target_pos = NULL){
   ls <- list()
   timewindow <- get_trial_timewindow(test, trialID)
-  if (is.null(timewindow)) return(NULL);
+  if (is.null(timewindow)) return(NULL)
   quest_log <- dt_player[Time > timewindow$start & Time < timewindow$finish, ]
   point_situation <- quest_log[Input == "Point", ]
+  
+  ls$target <- NA
   if(nrow(point_situation) < 1){
     smart_print(c("Warning", "get_trial_pointing", "no point event found"))
     ls$time <- NA
     ls$chosen <- NA
-    ls$target <- NA
   } else { 
     point_situation = point_situation[1]
     player_pos <- c(point_situation$Position.x, point_situation$Position.z)
-    target_pos <- get_goal_position(i_goal = get_goal_index(test, trialID), test = test, onlyXY = T)
     ls$time <- point_situation$Time
     ls$chosen <- point_situation$Rotation.X
-    ls$target <- angle_from_positions(player_pos, target_pos)
+    if (!is.null(target_pos)){
+      ls$target <- angle_from_positions(player_pos, target_pos)
+    }
   }
   return(ls)
 }
